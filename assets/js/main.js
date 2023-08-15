624537e45cd4b6cf554aa138281b29fbbeeb16a9
Felipe Saavedra <fisaavedrae@gmail.com>
2023-08-15 01:50:35 -0400

    version final de grafico

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//Deje este json para poder parametrizar la cantidad de monedas a mostrar en el select
+//Deje este arreglo para poder parametrizar la cantidad de monedas a mostrar en el select, el tipo de monedas a ingresar deben coincidir con la pagina mindicador.cl
 let tipoMonedas = [
     {
         tipo: "uf",
@@ -15,13 +15,13 @@ let tipoMonedas = [
 ];
 
 async function getMonedas() {
+    //Obtengo todas las monedas segun arreglo y lleno el select
     fetch('https://mindicador.cl/api').then(function (response) {
         return response.json();
     }).then(function (dailyIndicators) {
         let select = document.getElementById("monedaTO");
         tipoMonedas.forEach((element) => {
             let option = document.createElement("option");
-
             option.value = dailyIndicators[element.tipo].valor;
             option.text = element.descripcion;
             select.appendChild(option);
@@ -42,67 +42,74 @@ function convertirMoneda() {
         return;
     }
     let resultado = Number(moneda) * Number(monedaDestino);
-    console.log("moneda: " + moneda + " monedaDestino: " + monedaDestino + " resultado: " + resultado);
     document.getElementById("resultado").innerHTML = "Resultado: $" + resultado;
-    getMonedaMes()
+    getMonedaMes(); //Llamo a la funcion para obtener los valores de los 10 ultimos dias
 }
 
 async function getMonedaMes() {
     let monedaDestino = document.getElementById("monedaTO");
     let labels = [];
     let valores = [];
+    let contador = 0;
     descMoneda = monedaDestino.options[monedaDestino.selectedIndex].text;
     const url = "https://mindicador.cl/api/" + descMoneda.toLowerCase();
     const res = await fetch(url);
     const monedaJSON = await res.json();
 
-    //console.log(monedaJSON);
-
-    let contador = 0;
     monedaJSON.serie.forEach(element => {
-
         contador++;
         if (contador <= 10) {
             const d = new Date(element.fecha);
-            //console.log(d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear());
-            labels.push(d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear());
-            valores.push(element.valor);
+            labels.push(d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear()); //Cargo arreglo de fechas para ser usado en el grafico
+            valores.push(element.valor); //Cargo arreglo de valores para ser usado en el grafico
         }
     });
-    console.log(contador);
-    cargarChart(labels, valores, descMoneda);
+
+
+    cargarChart(labels, valores, descMoneda); //Llamo a la funcion para cargar el grafico
 }
-
+let myChart;
 function cargarChart(labels, valores, descMoneda) {
-    console.log(labels);
-    console.log(valores);
+    const ctx = document.getElementById('myChart');
 
-    var options = {
-        chart: {
-            type: 'line'
-        },
-        title: {
-            text: 'Valores ' + descMoneda,
-            style: {
-                fontSize: '24px',
-                fontWeight: 'bold',
-                fontFamily: 'Space Mono, monospace',
-                color: '#263238'
-            },
-        },
-        series: [{
-            name: 'Valores ' + descMoneda,
-            data: valores
-        }],
-        xaxis: {
-            categories: labels
-        }
+    if (myChart) {
+        myChart.destroy(); //Destruyo el canvas para volver a usar el grafico
     }
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Valores ' + descMoneda + ' últimos 10 dias',
+                data: valores,
+                borderWidth: 1,
+                Color: 'hsl(185, 41%, 84%)',
+                backgroundColor: [
+                    'hsl(172, 67%, 45%)'
+                ]
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
 
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-
-    chart.render();
-    chart.render();
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    labels: {
+                        color: 'hsl(183, 100%, 15%)',
+                        font: {
+                            size: 20,
+                            family: 'Space Mono'
+                        }
+                    }
+                }
+            }
+        }
+    });
 
 }
 getMonedas()
